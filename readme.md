2feb9c2c956c900dcb24a0c2ec9988b1e693aba4
PaulAyling <jimmyukka@gmail.com>
2021-04-21 12:51:13 +0200

    fixed up the import script 1

--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@
 
 #1. change G_save_path to your directory where you would like the files to go
 #2. get your api key from 'x-rapidapi-key': from rapidapi.com
+#4. Create a data directory in the root (for your new data files)
 #3. Run the code!
 
 # HOW TO CONTRIBUTE
--- src/common/fileManagement.py
+++ src/common/fileManagement.py
@@ -5,27 +5,28 @@ import json
 # Global VARS
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
-env_path = '../../../env'
+# env_path = '../../../env'
+# env_path = 'env'
 
 def sayHello():
     print('hello')
 
-# def removeCurrent(fileName, save_path):
-#     # used in outputToFile()
-#     completeName = os.path.join(save_path, fileName)
-#     if os.path.exists(completeName):
-#         os.remove(completeName)
-#         return 'File: Overwritten'
-#     else:
-#         return "File: Created"
+def removeCurrent(completeName):
+    # used in outputToFile()
+    if os.path.exists(completeName):
+        os.remove(completeName)
+        return 'File: Overwritten'
+    else:
+        return "File: Created"
     
-# def JSONExport(fileName,mylist,save_path): 
-#     completeName = os.path.join(save_path, fileName)  
-#     overwriteStatus = removeCurrent(completeName)
-#     with open(completeName, 'w') as f:
-#         json.dump(mylist, f)
-#     print('Records Exported:',len(mylist),'records' )
-#     print(current_time, 'Exported file:',completeName,overwriteStatus,)
+def JSONExport(fileName,save_path,mylist): 
+    completeName = os.path.join(save_path, fileName) 
+    print(completeName) 
+    overwriteStatus = removeCurrent(completeName)
+    with open(completeName, 'w') as f:
+        json.dump(mylist, f)
+    print('Records Exported:',len(mylist),'records' )
+    print(current_time, 'Exported file:',completeName,overwriteStatus,)
 
 def JSONImport(fileName,save_path): 
     completeName = os.path.join(save_path, fileName) 
