f9fe4f544cc0635eceece5574ba003df5bea308c
PaulAyling <jimmyukka@gmail.com>
2021-04-25 13:54:30 +0200

    updated stats and now working on dataframe build script (it is not working properly)

--- src/common/dataQuality.py
+++ src/common/dataQuality.py
@@ -1,3 +1,24 @@
-# DATA QUALITY
+# LISTS
+def cntPagedData(pagedData):
+    print('Nbr Pages:          ', len(pagedData))
+    print('Nbr items per page: ', len(pagedData[0]))
+
+
+
+
+
+# DATA QUALITY (DATAFRAME)
+def cnt(dataframe):
+    result  = len(df.index)
+    print('Nbr Rows:                ',result)
+    return result
+def cntDistinct(dataframe,col):
+    result = len(dataframe[col].unique())
+    print('Nbr Rows(Distinct Coins):',result)
+    return result
+
 def dataQualityReport(dataframe):
-    print('Total Number Of Records'len(df.index))
+    cnt(dataframe)
+    cntDistinct(dataframe,'name')
+    # desc = dataframe["current_price"].describe()
+    # print(desc)
